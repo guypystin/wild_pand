59693b57a8c079ca034e5b7d744d07de015b6ed5
guypystin <51515987+guypystin@users.noreply.github.com>
2021-04-01 13:23:45 +0300

    Удалил render.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+import './index.css';
+
+import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from './render'
-import state from './redux/state';
+import store from './redux/state';
 
-rerenderEntireTree(state);
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App state = {store.getState()} addPost={store.addPost.bind(store)} 
+        addMessage={store.addMessage.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} 
+        updateNewMessageText={store.updateNewMessageText.bind(store)}/>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
+
 reportWebVitals();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,67 +1,76 @@
-import { rerenderEntireTree } from "../render";
-
-let state = {
-    profilePage: {
-        postsData : [
-            {id:1, message: "Первое сообщение", likes:20},
-            {id:2, message: "Второе сообщение", likes:15},
-            {id:3, message: "Третье сообщение", likes:15},
-            {id:4, message: "Четвертое сообщение", likes:15},
-            {id:5, message: "Пятое сообщение", likes:15},
-            {id:5, message: "Шестое сообщение", likes:15},
-            {id:5, message: "Неважно какое сообщение", likes:15},
-        ],  
-        newPostText : '',
+let store = {
+    _state : {
+        profilePage: {
+            postsData : [
+                {id:1, message: "Первое сообщение", likes:20},
+                {id:2, message: "Второе сообщение", likes:15},
+                {id:3, message: "Третье сообщение", likes:15},
+                {id:4, message: "Четвертое сообщение", likes:15},
+                {id:5, message: "Пятое сообщение", likes:15},
+                {id:5, message: "Шестое сообщение", likes:15},
+                {id:5, message: "Неважно какое сообщение", likes:15},
+            ],  
+            newPostText : '',
+        },
+        dialogsPage: {
+            dialogsData : [
+                {id:1, name:'Андрей'},
+                {id:2, name:'Эдуард'},
+                {id:3, name:'Петр'},
+                {id:4, name:'Николай'},
+                {id:5, name:'Алексей'},
+                {id:6, name:'Кирилл'},
+                {id:7, name:'Андрей'},
+                {id:8, name:'Андрей'},
+              ],
+              messagesData : [
+                {id:1, message:'Привет'},
+                {id:2, message:'Привет, как дела?'},
+                {id:3, message:'Отлично, а у тебя?'},
+                {id:4, message:'Тоже'},
+                {id:5, message:'отлично'},
+              ],
+              newMessageText : '',
+        }
+        
+          
     },
-    dialogsPage: {
-        dialogsData : [
-            {id:1, name:'Андрей'},
-            {id:2, name:'Эдуард'},
-            {id:3, name:'Петр'},
-            {id:4, name:'Николай'},
-            {id:5, name:'Алексей'},
-            {id:6, name:'Кирилл'},
-            {id:7, name:'Андрей'},
-            {id:8, name:'Андрей'},
-          ],
-          messagesData : [
-            {id:1, message:'Привет'},
-            {id:2, message:'Привет, как дела?'},
-            {id:3, message:'Отлично, а у тебя?'},
-            {id:4, message:'Тоже'},
-            {id:5, message:'отлично'},
-          ],
-          newMessageText : '',
-    }
-    
-      
-}
-export let addMessage = (textMessage) => {
-    let newMessage = {
-        id:1,
-        message: textMessage,
-        likesCount: 1,
-    };
-    state.dialogsPage.messagesData.push(newMessage);
-    rerenderEntireTree(state)
-}
-export let addPost = (postMessage) => {
+    getState() {
+        return this._state;
+    },
+    _callSubscriber(){
+    },
+    addMessage(textMessage){
+        let newMessage = {
+            id:1,
+            message: textMessage,
+            likesCount: 1,
+        };
+        this._state.dialogsPage.messagesData.push(newMessage);
+        this._callSubscriber(this._state)
+    },
+    addPost(postMessage){
         let newPost = {
             id:5,
             message: postMessage,
             likesCount : 1,
         };
-        state.profilePage.postsData.push(newPost);
-        rerenderEntireTree(state)
+        this._state.profilePage.postsData.push(newPost);
+        this._callSubscriber(this._state)
+    },
+    updateNewPostText(newText){
+        this._state.profilePage.newPostText = newText ;
+        this._callSubscriber(this._state)
+    },
+    updateNewMessageText(newText){
+        this._state.dialogsPage.newMessageText = newText ;
+        console.log(newText)
+        this._callSubscriber(this._state)
+    },  
+    subscribe(observer){
+        this._callSubscriber = observer ;
+    },
 }
 
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText ;
-    rerenderEntireTree(state)
-}
-export let updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText ;
-    console.log(newText)
-    rerenderEntireTree(state)
-}
-export default state ;
+export default store ;
+window.store = store;
